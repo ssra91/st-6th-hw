e3ceaaeef445dd02ecdc9dca15454127bc66c2e7
sorakim <sora6418@gmail.com>
2024-05-30 12:47:55 +0900

    feat: ex1 문제

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,7 @@
 // TODO: input 타이핑할 때 리스트 컴포넌트가 불필요하게 렌더링되지 않게 해보세요.
+import React from "react";
+
+// eslint-disable-next-line react/prop-types,react-refresh/only-export-components
 const List = ({ items }) => {
   console.log("List component rendered");
   return (
@@ -10,4 +13,5 @@ const List = ({ items }) => {
   );
 };
 
-export default List;
+// eslint-disable-next-line react-refresh/only-export-components
+export default React.memo(List);
